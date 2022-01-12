d06304ca4ce811717eb636dfd82641079b9d385f
Nadir <nadirbekovnadir@yandex.ru>
2022-01-12 17:57:16 +0300

    Заполнители видео

--- MVVM/Services/VideoProcessingHandler.cpp
+++ MVVM/Services/VideoProcessingHandler.cpp
@@ -6,14 +6,24 @@ VideoProcessingHandler::VideoProcessingHandler(
     bool withProcessing)
     : BaseProcessingHandler<int, float, ProcessedVideo>(move(provider), move(processor), withProcessing)
 {
+    film1 = new QMovie("C:/Users/Nadir/Documents/Repositories/PultInterface/film1.gif");
+    film2 = new QMovie("C:/Users/Nadir/Documents/Repositories/PultInterface/film2.gif");
+
+    film1->start();
+    film2->start();
 }
 
 bool VideoProcessingHandler::convertProcessedToOutput(const float &data, ProcessedVideo &result)
 {
     //Пока что тут будет мегакостыль, так как логика остальных частей еще не готова
 
-    QImage frame(640, 480, QImage::Format_RGB32);
-    frame.fill(QColor(0, 0, int(data) % 255));
+    //QImage frame(640, 480, QImage::Format_RGB32);
+    //frame.fill(QColor(0, 0, int(data) % 255));
+
+    if (film1->state() != QMovie::Running)
+        film1->start();
+
+    QImage frame(std::move(film2->currentImage()));
 
     result.frame = std::move(frame);
     result.boundBoxes.push_back(QRect(100, 80, 50, 50));
@@ -26,8 +36,13 @@ bool VideoProcessingHandler::convertInputToOutput(const int &data, ProcessedVide
 {
     //Пока что тут будет мегакостыль, так как логика остальных частей еще не готова
 
-    QImage frame(640, 480, QImage::Format_RGB32);
-    frame.fill(QColor(0, 10 * data % 255, 0));
+    //QImage frame(640, 480, QImage::Format_RGB32);
+    //frame.fill(QColor(0, 10 * data % 255, 0));
+
+    if (film2->state() != QMovie::Running)
+        film2->start();
+
+    QImage frame(std::move(film1->currentImage()));
 
     result.frame = std::move(frame);
 
--- main.qml
+++ main.qml
@@ -6,23 +6,23 @@ import "MVVM/Views/BtmScreen"
 import "MVVM/Views/Common"
 
 Item{
-//    TopScreen{
-//        context: topScreenViewModel
-//    }
-//    BtmScreen{
-//        context: botScreenViewModel
-//    }
-
-    Window {
-        id: testWindow
-        width: 840
-        height: 680
-        visible: true
-
-        RouteMap {
-            width: 480
-            height: 360
-            context: botScreenViewModel.mapViewModel
-        }
+    TopScreen{
+        context: topScreenViewModel
     }
+    BtmScreen{
+        context: botScreenViewModel
+    }
+
+//    Window {
+//        id: testWindow
+//        width: 840
+//        height: 680
+//        visible: true
+
+//        RouteMap {
+//            width: 480
+//            height: 360
+//            context: botScreenViewModel.mapViewModel
+//        }
+//    }
 }
